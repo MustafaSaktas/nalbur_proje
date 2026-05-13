efd0c4ff78a1fd5341e788f15b7daee60eeb1e08
MustafaSaktas <msaktas42@hotmail.com>
2026-05-14 01:32:34 +0300

    Dosya düzenlemeleri

--- baglan.php
+++ baglan.php
@@ -3,7 +3,7 @@
 $host = '127.0.0.1'; // 'localhost:8889' yerine bunu dene
 $dbname = 'nalbur_db';
 $kullanici = 'root';
-$sifre = 'root'; 
+$sifre = ''; 
 
 try {
     $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $kullanici, $sifre);
